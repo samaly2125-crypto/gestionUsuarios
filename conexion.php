4ee78bfd1540b371313136074151d5dac9220caf
samaly2125-crypto <samaly.2125@gmail.com>
2026-04-29 19:16:36 -0500

    cambio4

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "gestion_usuarios");
+$conexion = new mysqli("mysql-marcela.alwaysdata.net", "marcela", "clase1234", "marcela_gestion_usuario");
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
